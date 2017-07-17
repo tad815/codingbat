105b0e861c0f2d575ae9f8e5ecc05057695e3f73
tad815 <arillinoming@gmail.com>
2017-07-17 00:55:00 -0500

    Add files via upload

---
+++ java/Recursion-1/bunnyEars2.java
@@ -0,0 +1,10 @@
+public int bunnyEars2(int bunnies) {
+  if(bunnies == 0){
+    return 0;
+  }
+  if(bunnies % 2 == 0){
+    return bunnyEars2(bunnies-1)+3;
+  }else{
+    return bunnyEars2(bunnies-1)+2;
+  }
+}
