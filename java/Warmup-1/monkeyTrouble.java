6b561f4c31c64511efbcbcd3c096c80eb1d98708
tad815 <arillinoming@gmail.com>
2017-07-16 23:31:45 -0500

    Add files via upload

---
+++ java/Warmup-1/monkeyTrouble.java
@@ -0,0 +1,9 @@
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  if(aSmile == true && bSmile == true){
+    return true;
+  }else if(aSmile == false && bSmile == false){
+    return true;
+  }else{
+    return false;
+  }
+}
